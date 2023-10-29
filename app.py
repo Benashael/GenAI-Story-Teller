6cf3f4aed974e11754efcbd150bdf034421d1832
Ben Ashael <114052702+Benashael@users.noreply.github.com>
2023-10-29 09:36:03 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,52 +1,23 @@
 import streamlit as st
-from transformers import AutoModelForCausalLM, AutoTokenizer
-import torch 
+from transformers import pipeline
 
-st.title("GenAI Storyteller")
+# Create a text generation pipeline
+story_gen = pipeline("text-generation", "pranavpsv/gpt2-genre-story-generator")
 
-# Load the pre-trained model and tokenizer
-model_name = "mosaicml/mpt-7b-storywriter"
-model = AutoModelForCausalLM.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+# Streamlit app title and description
+st.title("AI Story Generator")
+st.write("Generate creative stories with the power of AI!")
 
-# Input text to generate a story
-user_input = st.text_area("Enter a text prompt for the story:")
+# Input prompt
+user_input = st.text_area("Enter a story prompt:", "<BOS> Once upon a time, in a galaxy far, far away...")
 
-# Maximum word length for the generated story
-max_word_length = st.slider("Maximum Word Length for Story", min_value=50, max_value=500, value=250)
-
-# Number of story continuations to generate
-num_continuations = st.slider("Number of Story Continuations", min_value=1, max_value=5, value=1)
-
-@st.cache_data()
-def generate_stories(prompt, num_continuations, max_word_length):
-    generated_stories = []
-    for _ in range(num_continuations):
-        input_ids = tokenizer.encode(prompt, return_tensors="pt", max_length=100, truncation=True)
-        generated_story = model.generate(input_ids, max_length=max_word_length * 5, num_return_sequences=1, no_repeat_ngram_size=2)
-        generated_text = tokenizer.decode(generated_story[0], skip_special_tokens=True)
-        # Trim the generated story to the specified word length
-        generated_text = " ".join(generated_text.split()[:max_word_length])
-        generated_stories.append(generated_text)
-    return generated_stories
-
+# Generate and display the story
 if st.button("Generate Story"):
-    if user_input:
-        generated_stories = generate_stories(user_input, num_continuations, max_word_length)
-        # Display the generated stories
-        for i, story in enumerate(generated_stories):
-            st.subheader(f"Story {i+1}")
-            st.write(story)
-    else:
-        st.warning("Please enter a prompt to generate a story.")
+    with st.spinner("Generating story..."):
+        generated_story = story_gen(user_input, max_length=300, num_return_sequences=1, do_sample=True)[0]['generated_text']
+    st.write("Generated Story:")
+    st.write(generated_story)
 
-if st.button("Save Story"):
-    # Save the generated story to a text file
-    if generated_stories:
-        with open("generated_story.txt", "w") as file:
-            file.write(f"Maximum Word Length: {max_word_length}\n\n")
-            for i, story in enumerate(generated_stories):
-                file.write(f"Story {i+1}:\n")
-                file.write(story)
-                file.write("\n\n")
-        st.success("Story saved to 'generated_story.txt'")
+# Footer and acknowledgment
+st.sidebar.text("Powered by Hugging Face Transformers")
+st.sidebar.markdown("[pranavpsv/gpt2-genre-story-generator](https://huggingface.co/pranavpsv/gpt2-genre-story-generator)")
