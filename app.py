835a5dec3c12901f5cc6643efbea60c1cb525dfe
Ben Ashael <114052702+Benashael@users.noreply.github.com>
2023-12-09 07:16:26 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,49 @@
 import streamlit as st
-from transformers import pipeline
+import openai
 
-# Create a text generation pipeline
-story_gen = pipeline("text-generation", "pranavpsv/gpt2-genre-story-generator")
+# Function to generate stories based on user inputs
+def generate_stories(api_key, genre, tone, max_words, prompt, num_stories, purpose):
+    # Adjust word count to the nearest 10
+    max_words = round(max_words / 10) * 10
+    
+    # Prompt engineering
+    user_prompt = f"Write a {tone} {genre} story, up to {max_words} words: {prompt} for {purpose}"
+    
+    # Generate stories using OpenAI GPT-3.5
+    try:
+        openai.api_key = api_key
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=user_prompt,
+            max_tokens=max_words,
+            n=num_stories,
+            stop=None,
+        )
+        return [choice.text.strip() for choice in response.choices]
+    except openai.error.OpenAIError as e:
+        return [f"Error: {str(e)}"] * num_stories
 
-# Streamlit app title and description
-st.title("AI Story Generator")
-st.write("Generate creative stories with the power of AI!")
+# Streamlit app
+st.title("GenAI Story Teller")
 
-# Input prompt
-user_input = st.text_area("Enter a story prompt:", "<BOS> Once upon a time, in a galaxy far, far away...")
+# User inputs
+api_key = st.text_input("Enter OpenAI API Key:")
+genre = st.selectbox("Select Genre:", ["Sci-Fi", "Mystery", "Fantasy", "Adventure", "Thriller", "Comedy"])
+tone = st.selectbox("Select Tone:", ["Positive", "Negative", "Neutral"])
+purpose = st.selectbox("Select Purpose:", ["Education", "Storytelling for Children", "Entertainment", "Inspiration"])
+max_words = st.slider("Select Maximum Words:", 100, 500, 250, 10)
+num_stories = st.slider("Select Number of Stories:", 1, 5, 3, 1)
+prompt = st.text_input("Enter Prompt (less than 15 words):")
 
-# Generate and display the story
-if st.button("Generate Story"):
-    with st.spinner("Generating story..."):
-        generated_story = story_gen(user_input, max_length=300, num_return_sequences=1, do_sample=True)[0]['generated_text']
-    st.write("Generated Story:")
-    st.write(generated_story)
-
-# Footer and acknowledgment
-st.sidebar.text("Powered by Hugging Face Transformers")
-st.sidebar.markdown("[pranavpsv/gpt2-genre-story-generator](https://huggingface.co/pranavpsv/gpt2-genre-story-generator)")
+# Generate stories on button click
+if st.button("Generate Stories"):
+    if api_key and genre and tone and max_words and prompt and len(prompt.split()) <= 15:
+        # Call the generate_stories function
+        generated_stories = generate_stories(api_key, genre, tone, max_words, prompt, num_stories, purpose)
+        
+        # Display generated stories or errors
+        for i, story in enumerate(generated_stories):
+            st.subheader(f"Generated Story {i + 1} for {purpose}:")
+            st.write(story)
+    else:
+        st.error("Please fill in all fields correctly.")
