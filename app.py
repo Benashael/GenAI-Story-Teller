86ef35c3ce1311be47080deb0ce5f37150409c61
Ben Ashael <114052702+Benashael@users.noreply.github.com>
2023-12-24 19:58:29 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,60 +1,37 @@
 import streamlit as st
-import openai
-#from openai import Completion
-import os
+import torch
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+
+# Load the fine-tuned GPT-2 model and tokenizer
+model_path = "./fine-tuned-gpt2"
+model = GPT2LMHeadModel.from_pretrained(model_path)
+tokenizer = GPT2Tokenizer.from_pretrained(model_path)
 
 # Function to generate stories based on user inputs
-def generate_stories(api_key, genre, tone, max_words, prompt, num_stories, purpose):
-    # Adjust word count to the nearest 10
-    max_words = round(max_words / 10) * 10
+def generate_story(genre, tone, max_words, prompt, purpose):
+    # Combine user inputs to create a prompt
+    user_prompt = f"Genre: {genre}, Tone: {tone}, Max Words: {max_words}, Prompt: {prompt}, Purpose: {purpose}. "
     
-    # Prompt engineering
-    user_prompt = f"Write a {tone} {genre} story, up to {max_words} words: {prompt} for {purpose}"
+    # Tokenize the prompt
+    input_ids = tokenizer.encode(user_prompt, return_tensors="pt", max_length=100, truncation=True)
     
-    try:
-        openai.api_key = api_key
-        response = openai.Answer.create(
-            engine="text-davinci-003",
-            prompt=user_prompt,
-            max_tokens=max_words,
-            n=num_stories,
-            stop=None,
-        )
-        return [choice.text.strip() for choice in response.choices]
-    #except openai.error.OpenAIError as e:
-        #print(f"OpenAI API Error: {e}")
-        #return [f"Error: {str(e)}"] * num_stories
-    except Exception as e:
-        import traceback
-        traceback.print_exc()
-        print(f"An unexpected error occurred: {e}")
-        return [f"Error: An unexpected error occurred."] * num_stories
+    # Generate a story using the fine-tuned model
+    output = model.generate(input_ids, max_length=150, num_return_sequences=1, no_repeat_ngram_size=2)
+    
+    # Decode and return the generated story
+    generated_story = tokenizer.decode(output[0], skip_special_tokens=True)
+    return generated_story
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
-#openai.api_key = openai.OpenAI(temperature=0.7, openai_api_key=os.getenv('OPENAI_API_KEY'))
-
-# Streamlit app
-st.title("GenAI Story Teller")
-
-# User inputs
-api_key = st.secrets["API_KEY"]
-#api_key = os.environ.get(API_KEY)
-genre = st.selectbox("Select Genre:", ["Sci-Fi", "Mystery", "Fantasy", "Adventure", "Thriller", "Comedy"])
+# Streamlit UI
+st.title("GenAI Storyteller")
+genre = st.selectbox("Select Genre:", ["Sci-Fi", "Mystery", "Fantasy", "Adventure"])
 tone = st.selectbox("Select Tone:", ["Positive", "Negative", "Neutral"])
-purpose = st.selectbox("Select Purpose:", ["Education", "Storytelling for Children", "Entertainment", "Inspiration"])
-max_words = st.slider("Select Maximum Words:", 100, 500, 250, 10)
-num_stories = st.slider("Select Number of Stories:", 1, 5, 3, 1)
+max_words = st.slider("Select Maximum Words:", 0, 500, 250, 10)
 prompt = st.text_input("Enter Prompt (less than 15 words):")
+purpose = st.selectbox("Select Purpose:", ["Education", "Storytelling for Children", "Entertainment", "Inspiration"])
 
-# Generate stories on button click
-if st.button("Generate Stories"):
-    if api_key and genre and tone and max_words and prompt and len(prompt.split()) <= 15:
-        # Call the generate_stories function
-        generated_stories = generate_stories(api_key, genre, tone, max_words, prompt, num_stories, purpose)
-        
-        # Display generated stories or errors
-        for i, story in enumerate(generated_stories):
-            st.subheader(f"Generated Story {i + 1} for {purpose}:")
-            st.write(story)
-    else:
-        st.error("Please fill in all fields correctly.")
+# Button to generate and display the story
+if st.button("Generate Story"):
+    generated_story = generate_story(genre, tone, max_words, prompt, purpose)
+    st.subheader("Generated Story:")
+    st.write(generated_story)
